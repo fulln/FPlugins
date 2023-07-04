1b9b213eec388696afb48212022a1fff0b04fd70
fulln <245193247@qq.com>
2023-07-05 01:38:52 +0800

    修改 测试使用box

--- src/main/kotlin/com/github/fulln/fplugins/toolWindow/FPluginsToolWindowFactory.kt
+++ src/main/kotlin/com/github/fulln/fplugins/toolWindow/FPluginsToolWindowFactory.kt
@@ -10,6 +10,7 @@ import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
 import com.github.fulln.fplugins.MyBundle
 import com.github.fulln.fplugins.services.MyProjectService
+import com.intellij.ui.components.JBBox
 import javax.swing.JButton
 
 
@@ -32,18 +33,47 @@ class FPluginsToolWindowFactory : ToolWindowFactory {
         private val service = toolWindow.project.service<MyProjectService>()
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
-            var jbLabel = JBLabel(MyBundle.message("description"))
+            var description = JBLabel(MyBundle.message("description"))
 
             // add jblabel  to panel
-            add(jbLabel)
-            add(label)
+            add(description)
 
-            add(JButton(MyBundle.message("shuffle")).apply {
-                addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
-                }
-            })
+            // change line
+            add(JBLabel(""))
+            // add 下拉框 到 panel
+            add(JBLabel(MyBundle.message("selectConfig")))
+            //
+            val list = listOf("1", "2", "3")
+            var jbBox = JBBox.createHorizontalBox()
+            // add list  to jbbox
+            list.forEach() {
+                jbBox.add(JBLabel(it))
+            }
+
+            //  下拉框选中后，将选中的值传递给后台
+
+            //  后台根据选中的值，加载对应的配置文件
+            //  配置文件中的内容，加载到界面中
+
+
+
+
+                // add button to panel
+//           add(JButton(MyBundle.message("loadsConfig")).apply {
+//                addActionListener {
+//                    description.text = MyBundle.message("randomLabel", service.getRandomNumber())
+//                }
+//            }
+
+
+
+
+
+//            add(JButton(MyBundle.message("shuffle")).apply {
+//                addActionListener {
+//                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+//                }
+//            })
         }
     }
 }
